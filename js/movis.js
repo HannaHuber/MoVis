13382b971fda9cf529393e59ad142920ad5c076f
HannaHuber <hanna_huber@hotmail.com>
2017-06-04 21:21:51 +0200

    add movie info
    filter genre and/or year
    additional average (not working yet)

--- js/movis.js
+++ js/movis.js
@@ -2,7 +2,10 @@
  * Created by Hanna on 03.06.2017.
  */
 
-var selectedID = document.getElementById("dropId").value;
+var selectedYear = document.getElementById("dropdownYear").value;
+var selectedGenre = document.getElementById("dropdownGenre").value;
+var selectedGender = document.getElementById("dropdownGender").value;
+var isAverage = document.getElementById("checkboxAverage").checked;
 
 var svg = d3.select("svg"),
     margin = { top: 20, right: 80, bottom: 30, left: 50 },
@@ -20,45 +23,61 @@ var line = d3.line()
 
 var movieLine,
 	movieLabel;
+var pdf;
+loadData(init);
 
-loadData(parseAge, init);
 
 
 function updateFilter(){
-    selectedID = document.getElementById("dropId").value;
-    console.log(selectedID);
+    selectedYear = document.getElementById("dropdownYear").value;
+    console.log(selectedYear);
     //movieLabel.remove();
     //movieLine.remove();
-    loadData(parseAge,init);
+    //loadData(parseAge,update);
+    update();
 }
 
 function parseAge(data, _, columns) {
     data.age = +data.age;
     for (var i = 1, t = 0, c; i < columns.length; ++i) {
+        // start movie id at 0
         data[c = columns[i]] =  +data[c];
     }
     return data;
 }
 
-function parseInfo(data, _, columns) {
+function parseInfo(data, i, columns) {
+    data.id = i;
     data.year = +data.year;
     data["title"] = data["title"];
-    data["genres"] = data["genres"];
+    data["genres"] = data["genres"].split("|");
+
+    var dropdown = document.getElementById("dropdownYear");
+    var option = document.createElement("option");
+    option.text = data.year;
+    dropdown.add(option);
+
     return data;
 }
 
-function init(error, data) {
+function init(error, data, info) {
     if (error) { console.log(error); }
-    console.log(data[0]);
-	console.log(selectedID);
-    var pdf = data.columns.slice(1,100).map(function (id) {
+    console.log(data[3]);
+    console.log(info[3]);
+    console.log(info[3]["genres"]);
+    pdf = data.columns.slice(1).map(function (id) {
         return {
-            id: id,
+            id: id-1,
+            title: info[id-1].title,
+            year: info[id-1].year,
+            genres: info[id-1].genres,
             values: data.map(function (d) {
                 return { age: d.age, density: d[id]/d3.sum(data,function(d){return d[id]}) };
             })
         };
     });
+    console.log(pdf[3]);
+
 
     x.domain(
         d3.extent(data, function (d) { return d.age; })
@@ -92,13 +111,13 @@ function init(error, data) {
 
 
     // movie lines
+    var selection = getSelection();
      var movie = g.selectAll(".movie")
-        .data(pdf)
+        .data(selection)
         .enter().append("g")
-        .filter(function(o){return o.id==selectedID})
         .attr("class", "movie");
 
-     movieLine =
+     //movieLine =
         movie.append("path")
         .attr("class", "line")
         .attr("d", function (d) { return line(d.values); })
@@ -106,21 +125,75 @@ function init(error, data) {
             d3.select(this).style({"stroke-opacity":"0.5","stroke-width":"0.5px"});
         })
         .on("mouseover", function(){
-            d3.select(this).style({"stroke-opacity":"1","stroke-width":"1px"});
+            d3.select(this)
+                .style({"stroke-opacity":"1","stroke-width":"1px"});
         });
 
-    movieLabel =
+    //movieLabel =
         movie.append("text")
         .datum(function (d) { return { id: d.id, value: d.values[d.values.length - 1] }; })
         .attr("transform", function (d) { return "translate(" + x(d.value.age) + "," + y(d.value.density) + ")"; })
         .attr("x", 3)
         .attr("dy", "0.35em")
         .style("font", "10px sans-serif")
-        .text(function (d) { return d.id; });
+        .text(function (d) {
+            return "id:" + d.id + ", title:" + d["title"];
+        });
 
 }
-/*
+
+function getSelection() {
+    var selection = pdf;
+    // filter
+    if (selectedYear != "all") {
+        selection = selection.filter(function (c) {
+            return c.year == selectedYear
+        })
+    }
+
+    if (selectedGenre != "all") {
+        selection = selection.filter(function (c) {
+            return c.genres.includes(selectedGenre);
+        })
+    }
+
+    // average
+    if (isAverage) {
+        selection =  selection.map(function (c) {
+            var meanDensities = [];
+            for (var k = 0; k < c.values.length; ++k) {
+                meanDensities[k] = d3.mean(selection.map(function (c) {
+                    return c.values;}).map(function(d,i){return d[k];}), function(d){return d.density;});
+            }
+            var tmp = c.values.map(function(d,i) {
+                return {
+                    age:d.age,
+                    density: meanDensities
+                };
+            });
+            return {
+                id:"-",
+                title:"-",
+                year:selectedYear,
+                genres:selectedGenre,
+                values: tmp
+                };
+        });
+    }
+    return selection;
+}
+
 function update(error, data) {
+    var movie = g.selectAll(".movie")
+        .data(pdf.filter(function(o){return o.id==selectedYear}));
+
+        movie.exit().remove();
+    movie
+        .enter().append("g")
+        .attr("class", "movie");
+
+    /*
+    console.log(selectedYear);
     var pdf = data.columns.slice(1,100).map(function (id) {
         return {
             id: id,
@@ -133,7 +206,7 @@ function update(error, data) {
     y.domain([
         d3.min(pdf, function (c) { return d3.min(c.values, function (d) { return d.density; }); }),
         d3.max(pdf, function (c) { return d3.max(c.values, function (d) { return d.density; }); })
-    ]);
+    ]);*/
     // Select the section we want to apply our changes to
     var svg = d3.select("svg").transition();
 
@@ -148,32 +221,34 @@ function update(error, data) {
         .duration(750)
         .call(d3.svg.axis().scale(y).orient("left"));
 
-}*/
+}
 
 /*function updateFilter(){
-    selectedID = document.getElementById("dropId").value;
-	console.log(selectedID);
+    selectedYear = document.getElementById("dropId").value;
+	console.log(selectedYear);
     loadData(parseAge,init);
 }*/
 
-function loadData(f1, f2){
-    d3.request("./data/age_all_id_1-15k.csv")
+function loadData(f2){
+    /*d3.request("./data/age_all_id_1-15k.csv")
         .mimeType("text/plain")
         .response(function(xhr) {return d3.dsvFormat(";").parse(xhr.responseText, f1)})
-        .get(f2);
+        .get(f2);*/
 
 //d3.request("./raw/movie_meta.csv")
 //   .mimeType("text/plain")
 //   .response(function(xhr) {return d3.dsvFormat(";").parse(xhr.responseText, parseInfo
 
-    /*d3.queue()
-     //   .defer(readDSV, "./data/pdf/age_all_id_1-15k.csv", parseAge)
+    d3.queue()
+        .defer(d3.csv,"./data/age_all_id_1-15k_cs.csv", parseAge)
+         .defer(d3.csv,"./data/movie_info.csv",parseInfo)
+     // /*  .defer(readDSV, "./data/pdf/age_all_id_1-15k.csv", parseAge)
 //.defer(readDSV, "./data/meta/movie_meta.csv", parseInfo)
 //.awaitAll(draw);
 
 
 
-.defer(d3.csv,"./data/age_all_id_1-15k_cs.csv", f1)
+
 //   .defer(d3.csv,"./data/age_all_id_1-15k_cs_unknown15137.csv", f1)
 //.defer(d3.csv,"./data.csv", type)
 //.defer(d3.csv,"./data/pdf/originDF_all.csv")
@@ -182,8 +257,8 @@ function loadData(f1, f2){
 
 //.defer(d3.csv,"./data/pdf/ageDF_m.csv")
 //.defer(d3.csv,"./data/pdf/ageDF_f.csv")
-//.defer(d3.csv,"./data/pdf/genderDF.csv")
-.await(f2);*/
+//.defer(d3.csv,"./data/pdf/genderDF.csv")*/
+.await(f2);
 }
 
 
@@ -199,7 +274,7 @@ function readDSV(file, access) {
 
 /* Dropdown menu */
 //var selectID = document.getElementById("dropId");
-//var selectedID = selectID.value;
+//var selectedYear = selectID.value;
 
 /* When the user clicks on the button,
  toggle between hiding and showing the dropdown content
