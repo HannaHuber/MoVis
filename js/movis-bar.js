fd98a86884950f910ea585cda5474d24a09cac47
HannaHuber <hanna_huber@hotmail.com>
2017-06-11 22:06:20 +0200

    ignore line chart + change chart style
    don't load line script in index
    increase bar chart width
    set chart position to absolute

--- js/movis-bar.js
+++ js/movis-bar.js
@@ -3,35 +3,34 @@
  */
 
 function initBar(columns) {
+
+    // Chart dimension + position
     var svgBar = d3.select("#svgBar"),
         margin = { top: 10, right: 50, bottom: 50, left: 50 },
         width = svgBar.attr("width") - margin.left - margin.right,
         height = svgBar.attr("height") - margin.top - margin.bottom,
         g = svgBar.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // Axes range
     var x = d3.scaleBand()
         .rangeRound([0, width])
         .padding(0.01)
         .align(0.1);
     var bandWidth = 8;
-
     var y = d3.scaleLinear()
         .rangeRound([height, 0]);
-
-    var seq = [];
-    for (var i=0; i<8;++i){
-        seq[i] = d3.interpolateBlues(i/8);
+    var seqColors = [];
+    for (var i=0; i<13;++i){
+        seqColors[i] = d3.interpolateBlues(i/13);
     }
-    seq[8] =  d3.interpolateBlues(1);
-    var z = d3.scaleOrdinal()
-        .range(seq);
+    seqColors[13] =  d3.interpolateBlues(1);
+    var z = d3.scaleQuantize()
+        .range(seqColors);
 
+    // Stack data
     var stack = d3.stack()
         .offset(d3.stackOffsetExpand);
-
     var firstBinIdx = 1;
-
-
     var selection =
         d3.nest()
             .key(function (d){return d.year;})
@@ -54,10 +53,11 @@ function initBar(columns) {
                 return barData;
             });
 
-
+    // Axes domain
     x.domain(selection.map(function(d) { return d.year; }));
-    z.domain(columns.slice(firstBinIdx));
+    z.domain(d3.extent(columns.slice(2)));
 
+    // Bars
     var serie = g.selectAll(".serie")
         .data(stack.keys(columns.slice(firstBinIdx))(selection))
         .enter().append("g")
@@ -69,7 +69,6 @@ function initBar(columns) {
                 }
 
         });
-
     serie.selectAll("rect")
         .data(function(d) {
             return d; })
@@ -79,26 +78,25 @@ function initBar(columns) {
         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
         .attr("width", bandWidth);
 
+    // Append axes
     g.append("g")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(-" + bandWidth/2 +" ," + height + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
         .attr("transform", 'translate(-10,10)rotate(-45)');
-
     g.append("g")
         .attr("class", "axis axis--y")
         .call(d3.axisLeft(y).ticks(10, "%"));
 
+    // Append legend
     var legend = serie.append("g")
         .attr("class", "legend")
         .attr("transform", function(d) { var d = d[d.length - 1]; return "translate(" + (x(d.data.year) + bandWidth) + "," + ((y(d[0]) + y(d[1])) / 2) + ")"; });
-
     legend.append("line")
         .attr("x1", -6)
         .attr("x2", 6)
         .attr("stroke", "#000");
-
     legend.append("text")
         .attr("x", 9)
         .attr("dy", "0.35em")
